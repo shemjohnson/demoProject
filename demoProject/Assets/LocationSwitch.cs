542d3f9a550d93ee060fabdedd41cb6c78901685
JohnPeterson3 <johnny.student.1@gmail.com>
2020-10-28 15:03:33 -0600

    Can switch locations from under the canopy to outside the canopy.

---
+++ demoProject/Assets/LocationSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationSwitch : MonoBehaviour
+{
+	 public int counter = 0;
+	 
+	 public bool underCanopy;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		underCanopy = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		OVRInput.Update();
+        if(Input.GetKeyDown("m") || OVRInput.Get(OVRInput.RawButton.B) && counter > 20 ){
+			if(underCanopy)
+				this.transform.position += new Vector3(-6, 0, 0); 
+			else
+				this.transform.position += new Vector3(6, 0, 0);
+			counter = 0;
+			underCanopy = !underCanopy;
+		}
+		counter++;
+    }
+}
