cea743b20157e7f9ed2d05bba9eb83af8e17270d
Shem Johnson <shemjohnson836@gmail.com>
2020-10-29 11:53:07 -0600

    Teleporter

---
+++ demoProject/Assets/Teleporter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider Col) {
+        Col.transform.position = new Vector3 (Random.Range (-70.0f,70.0f), Col.transform.position.y, Random.Range (-70.0f, 70.0f));
+    }
+}
